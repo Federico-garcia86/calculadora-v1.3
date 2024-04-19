099bd226578983f30ca64ecdafc3e9ede41952bc
federico garcia <167465746+Federico-garcia86@users.noreply.github.com>
2024-04-19 04:06:04 -0300

    calculadora v1.3

--- calculadora v.1.3.c
+++ calculadora v.1.3.c
@@ -4,35 +4,35 @@
 
 int main(void)
 {
-int num1, num2,num3;
+int num1, num2,res;
 p("\nsuma:");
 p("\ningrese el valor: ");
 s("%d",&num1);
 p("\ningrese este valor:");
 s("%d",&num2);
-num3=num1+num2;
-p(" %d + %d = %d",num1,num2,num3);
+res=num1+num2;
+p(" %d + %d = %d",num1,num2,res);
 p("\nresta");
 p("\ningrese el valor: ");
 s("%d",&num1);
 p("\ningrese este valor:");
 s("%d",&num2);
-num3=num1-num2;
-p("\n%d-%d=%d",num1,num2,num3);
+res=num1-num2;
+p("\n%d-%d=%d",num1,num2,res);
 p("\ndivision");
 p("\ningrese el valor: ");
 s("%d",&num1);
 p("\ningrese este valor:");
 s("%d",&num2);
-num3=num1/num2;
-p("\n%d/%d= %d",num1,num2,num3);
+res=num1/num2;
+p("\n%d/%d= %d",num1,num2,res);
 p("\nmultiplicacion");
 p("\ningrese el valor: ");
 s("%d",&num1);
 p("\ningrese este valor:");
 s("%d",&num2);
-num3=num1*num2;
-p("\n%d * %d = %d",num1,num2,num3);
+res=num1*num2;
+p("\n%d * %d = %d",num1,num2,res);
 
 return 0;
 }
